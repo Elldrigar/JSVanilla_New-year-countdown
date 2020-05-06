c81ca4d685b6a215986957b29f64f5f9893c2649
Artur Gawron <artur@gawron.me>
2020-05-06 23:36:27 +0200

    Grab days, hours, minutes, seconds from DOM

---
+++ app.js
@@ -0,0 +1,5 @@
+const countdown = document.getElementById('countdown');
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
