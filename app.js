1403c8cdba834f44bd058512593569e6d4f370c5
Artur Gawron <artur@gawron.me>
2020-05-07 14:40:51 +0200

    Add Year to background

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
 const days = document.getElementById('days');
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const currentYear = new Date().getFullYear();
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+//Set background year
+year.innerText = currentYear + 1;
 
 function updateTime() {
 	const currentTime = new Date();
